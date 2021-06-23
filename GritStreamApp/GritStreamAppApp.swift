8c3d90c8c955918c2efea09fda33fc54f9e23c3d
Florian Feuerstein <florian@gritworld.com>
2021-06-23 15:47:07 +0200

    Initial Commit

---
+++ GritStreamApp/GritStreamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GritStreamAppApp.swift
+//  GritStreamApp
+//
+//  Created by Mac1 on 23.06.21.
+//
+
+import SwiftUI
+
+@main
+struct GritStreamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
